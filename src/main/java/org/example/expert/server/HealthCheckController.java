eb67ee1971866dc85586beeecdc54f8f6f64c193
minjonyyy <minjonyyy@naver.com>
2025-03-20 13:47:56 +0900

    (Lv3) 12-1. AWS ec2 연결 및 health check API 구축

---
+++ src/main/java/org/example/expert/server/HealthCheckController.java
@@ -0,0 +1,18 @@
+package org.example.expert.server;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("서버 연결 성공");
+    }
+
+}
